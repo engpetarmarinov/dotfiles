659464562c17b79eab047814127280d65b0af106
Petar Marinov <petar.marinov@acronis.com>
2025-02-16 18:20:19 +0200

    p10k

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -39,7 +39,7 @@ setopt HIST_REDUCE_BLANKS
 setopt complete_aliases
 
 # OhMyZsh
-export ZSH=$HOME/.oh-my-zsh/
+export ZSH=$HOME/.oh-my-zsh
 ZSH_THEME="powerlevel10k/powerlevel10k"
 plugins=(git zsh-autosuggestions zsh-syntax-highlighting)
 source $ZSH/oh-my-zsh.sh
--- zsh/install.sh
+++ zsh/install.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
-
+#!/bin/bash
+set -e
 # Install oh-my-zsh
 rm -rf "$HOME"/.oh-my-zsh
-ZSH=$HOME/.oh-my-zsh/ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+ZSH=$HOME/.oh-my-zsh KEEP_ZSHRC=yes sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions                                                                                                                                                                               ✔  09:40:09
-git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-$HOME~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
+git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
