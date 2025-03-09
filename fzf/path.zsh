3bf604a8355ffa3d4b47acfa4595112ec05ddfc4
Petar Marinov <eng.petar.marinov@gmail.com>
2025-03-09 18:36:42 +0200

    zsh path to work on linux

--- fzf/path.zsh
+++ fzf/path.zsh
@@ -1,1 +1,5 @@
-export PATH="/opt/homebrew/opt/fzf/bin:$PATH"
+if [[ "$(uname -s)" = "Darwin" ]]; then
+    export PATH="/opt/homebrew/opt/fzf/bin:$PATH"
+else
+    export PATH="$HOME/.fzf/bin:$PATH"
+fi
