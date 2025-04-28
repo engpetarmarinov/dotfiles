65752a118da7f4696764b331c990e1a180f0fcd0
Petar Marinov <eng.petar.marinov@gmail.com>
2025-04-28 21:34:22 +0300

    go path

--- go/path.zsh
+++ go/path.zsh
@@ -1,1 +1,6 @@
-export PATH="$HOME/go/bin/:$PATH"
+if [[ "$(uname -s)" = "Darwin" ]]; then
+    export PATH="$HOME/go/bin/:$PATH"
+else
+    export PATH="$PATH:/usr/local/go/bin:$HOME/go/bin/"
+fi
+
