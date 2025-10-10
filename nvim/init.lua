b7b197b2e7a65275583104c3c7a987ca4374d9a8
Petar Marinov <petar.marinov@acronis.com>
2025-10-10 20:33:45 +0300

    add copilot plugins to nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -448,7 +448,121 @@ require("lazy").setup({
 			-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
 		},
 	},
+	-- copilot
+	{
+		"zbirenbaum/copilot.lua",
+		cmd = "Copilot",
+		build = ":Copilot auth",
+		event = "BufReadPost",
+		opts = {
+			suggestion = {
+				enabled = not vim.g.ai_cmp,
+				auto_trigger = true,
+				hide_during_completion = vim.g.ai_cmp,
+				keymap = {
+					accept = "<Tab>", -- ✅ Accept with Tab
+					accept_word = false,
+					accept_line = false,
+					next = "<M-]>", -- optional: next suggestion
+					prev = "<M-[>", -- optional: previous suggestion
+					dismiss = "<C-]>", -- optional: dismiss
+				},
+			},
+			panel = { enabled = false },
+			filetypes = {
+				markdown = true,
+				help = true,
+			},
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			servers = {
+				-- copilot.lua only works with its own copilot lsp server
+				copilot = { enabled = false },
+			},
+		},
+	},
+	-- copilot cmp integrates copilot wiwth nvim-cmp
+	{
+		"zbirenbaum/copilot-cmp",
+		after = { "copilot.lua" },
+		opts = {},
+	},
+	-- copilot chat
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		branch = "main",
+		cmd = "CopilotChat",
+		opts = function()
+			local user = vim.env.USER or "User"
+			user = user:sub(1, 1):upper() .. user:sub(2)
+			return {
+				auto_insert_mode = true,
+				question_header = "  " .. user .. " ",
+				answer_header = "  Copilot ",
+				window = {
+					width = 0.4,
+				},
+			}
+		end,
+		keys = {
+			{ "<c-s>", "<CR>", ft = "copilot-chat", desc = "Submit Prompt", remap = true },
+			{ "<leader>a", "", desc = "+ai", mode = { "n", "v" } },
+			{
+				"<leader>aa",
+				function()
+					return require("CopilotChat").toggle()
+				end,
+				desc = "Toggle (CopilotChat)",
+				mode = { "n", "v" },
+			},
+			{
+				"<leader>ax",
+				function()
+					return require("CopilotChat").reset()
+				end,
+				desc = "Clear (CopilotChat)",
+				mode = { "n", "v" },
+			},
+			{
+				"<leader>aq",
+				function()
+					vim.ui.input({
+						prompt = "Quick Chat: ",
+					}, function(input)
+						if input ~= "" then
+							require("CopilotChat").ask(input)
+						end
+					end)
+				end,
+				desc = "Quick Chat (CopilotChat)",
+				mode = { "n", "v" },
+			},
+			{
+				"<leader>ap",
+				function()
+					require("CopilotChat").select_prompt()
+				end,
+				desc = "Prompt Actions (CopilotChat)",
+				mode = { "n", "v" },
+			},
+		},
+		config = function(_, opts)
+			local chat = require("CopilotChat")
 
+			vim.api.nvim_create_autocmd("BufEnter", {
+				pattern = "copilot-chat",
+				callback = function()
+					vim.opt_local.relativenumber = false
+					vim.opt_local.number = false
+				end,
+			})
+
+			chat.setup(opts)
+		end,
+	},
 	-- LSP Plugins
 	{
 		-- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
